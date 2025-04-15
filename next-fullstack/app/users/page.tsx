9e5bb602a4670b3dbaeee9ed1065fe11a3a0ddec
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2025-04-15 14:09:59 +0200

    Aufgabe 13

---
+++ next-fullstack/app/users/page.tsx
@@ -0,0 +1,15 @@
+export default async function UserPage() {
+  const response = await fetch('http://localhost:3001/users');
+  const users = (await response.json()) as { id: string; name: string }[];
+
+  return (
+    <>
+      <h1 className="text-4xl font-bold">User List</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
