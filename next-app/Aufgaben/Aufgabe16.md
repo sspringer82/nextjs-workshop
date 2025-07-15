a008f83d10fe134c7f6fd2a712ac59ca39772418
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2025-07-15 11:13:26 +0200

    Aufgabe 16

---
+++ next-app/Aufgaben/Aufgabe16.md
@@ -0,0 +1,10 @@
+# Styling
+
+Teste deine Stylingfähigkeiten
+
+- inline css
+- css import
+- css modules
+- sass `npm install sass`
+- css-in-js `npm install styled-components`
+- tailwind
--- next-app/src/app/styling/tailwind/page.tsx
+++ next-app/src/app/styling/tailwind/page.tsx
@@ -2,8 +2,8 @@ import { NextPage } from 'next';
 
 const StylingPage: NextPage = () => {
   return (
-    <div>
-      <h1 className="text-4xl font-extrabold text-center text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 mt-12 mb-8 drop-shadow-lg">
+    <div className="border-2 border-blue-500 shadow-lg m-8">
+      <h1 className="text-4xl font-extrabold text-center text-transparent bg-clip-text bg-gradient-to-r from-blue-700 via-purple-500 to-pink-600 mt-12 mb-8 drop-shadow-2xl">
         Hallo Welt
       </h1>
     </div>
