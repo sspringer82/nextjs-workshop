493bad678caaa3e93ecf581184e990d141740dfa
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2025-07-15 11:21:07 +0200

    styling

--- next-app/src/app/styling/page.tsx
+++ next-app/src/app/styling/page.tsx
@@ -1,15 +1,25 @@
 import { NextPage } from 'next';
 import Link from 'next/link';
+import './style.css';
 
 const StylingPage: NextPage = () => {
   return (
     <div>
       <ul>
         <li>
-          <Link href="/styling/css">Go to CSS Styling</Link>
+          <Link href="/styling/css">Inline CSS & CSS Import</Link>
         </li>
         <li>
-          <Link href="/styling/modules">Go to CSS Modules</Link>
+          <Link href="/styling/modules">CSS Modules</Link>
+        </li>
+        <li>
+          <Link href="/styling/sass">Sass</Link>
+        </li>
+        <li>
+          <Link href="/styling/css-in-js">CSS-in-JS</Link>
+        </li>
+        <li>
+          <Link href="/styling/tailwind">Tailwind CSS</Link>
         </li>
       </ul>
     </div>
