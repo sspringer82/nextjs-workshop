390e71506992a40d0bb955365324b60f54309cc9
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2025-11-26 10:05:34 +0100

    multiple server functions

--- next-app/src/actions/user.actions.ts
+++ next-app/src/actions/user.actions.ts
@@ -2,3 +2,7 @@
 export async function doItAction(who: string) {
   console.log('Hello ' + who);
 }
+
+export async function doSomethingElseAction() {
+  console.log('do Something Else');
+}
--- next-app/src/components/user/button.tsx
+++ next-app/src/components/user/button.tsx
@@ -1,17 +1,20 @@
 'use client';
 
-import { doItAction } from '@/actions/user.actions';
+import { doItAction, doSomethingElseAction } from '@/actions/user.actions';
 
 const Button: React.FC = () => {
   return (
-    <button
-      onClick={() => {
-        doItAction('World');
-        console.log('Hello Client');
-      }}
-    >
-      click me
-    </button>
+    <div>
+      <button
+        onClick={() => {
+          doItAction('World');
+          console.log('Hello Client');
+        }}
+      >
+        click me
+      </button>
+      <button onClick={() => doSomethingElseAction()}>do something else</button>
+    </div>
   );
 };
 
